654ca8bb3bd9ba5ee8352683351eb98560e3bffb
Yuki Abe <12268087+yukia3@users.noreply.github.com>
2020-10-05 15:17:02 +0900

    first commit

---
+++ install_on_cluster.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# install Google Python client on all nodes
+apt-get update
+apt-get install -y python-pip
+pip install --upgrade google-api-python-client
+
+# install Mecab
+apt-get install -y mecab libmecab-dev mecab-ipadic mecab-ipadic-utf8 git make curl xz-utils file swig
+git clone --depth 1 https://github.com/neologd/mecab-ipadic-neologd.git
+./mecab-ipadic-neologd/bin/install-mecab-ipadic-neologd -n -a -y
+pip install --upgrade mecab-python3
+
+# git clone on Master
+USER=CHANGE_TO_USER_NAME
+ROLE=$(/usr/share/google/get_metadata_value attributes/dataproc-role)
+REPOSITORY=CHANGE_TO_YOUR_REPOGITORY_URL
+if [["${ROLE}" == 'Master']]; then
+  cd home/$UESR
+  git clone ${REPOSITORY}
+fi
