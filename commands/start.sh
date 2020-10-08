4b66a3714b48e95addbe7905adb41278dd909c96
Yuki Abe <12268087+yukia3@users.noreply.github.com>
2020-10-08 19:09:44 +0900

    Support for mecabrc / Support BigQuery connector, JDBC #4 / #5

--- commands/start.sh
+++ commands/start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 REGION=asia-northeast1
 CLUSTER_NAME=cluster
-SHELL_INSTALL_ON_CLUSTER=gs://
-WORKER_BUCKET_NAME=
+SHELL_INSTALL_ON_CLUSTER=gs://${YOUR_GCS_BUCKET}/init/install_on_cluster.sh
+WORKER_BUCKET_NAME=${YOUR_GCS_BUCKET}
 gcloud dataproc clusters create ${CLUSTER_NAME} \
   --region ${REGION} \
   --optional-components=ANACONDA,JUPYTER \
@@ -12,5 +12,6 @@ gcloud dataproc clusters create ${CLUSTER_NAME} \
   --worker-machine-type=n1-standard-2 \
   --master-machine-type=n1-standard-4 \
   --initialization-actions ${SHELL_INSTALL_ON_CLUSTER} \
+  --properties=spark:spark.jars=/usr/local/lib/jars/*,spark:spark.driver.extraClassPath=/usr/local/lib/jars/* \
   --zone=asia-northeast1-b \
   --num-workers=2
--- init/install_on_cluster.sh
+++ init/install_on_cluster.sh
@@ -3,6 +3,7 @@
 # install Google Python client on all nodes
 apt-get update
 apt-get install -y python-pip
+pip install --upgrade pip
 pip install --upgrade google-api-python-client
 
 # install Mecab
@@ -10,6 +11,20 @@ apt-get install -y mecab libmecab-dev mecab-ipadic mecab-ipadic-utf8 git make cu
 git clone --depth 1 https://github.com/neologd/mecab-ipadic-neologd.git
 ./mecab-ipadic-neologd/bin/install-mecab-ipadic-neologd -n -a -y
 pip install --upgrade mecab-python3
+cp /etc/mecabrc /usr/local/etc/
+
+# JAR
+JAR_PATH=/usr/local/lib/jars
+mkdir $JAR_PATH
+
+## JDBC MySQL
+## ref. https://www.kwbtblog.com/entry/2019/07/27/003225
+URL_GCS_JDBC_MYSQL_JAR=gs://${YOUR_GCS_BUCKET}/jars/mysql-connector-java-8.0.21.jar
+gsutil cp $URL_GCS_JDBC_MYSQL_JAR $JAR_PATH/
+
+## BigQuery
+URL_GCS_BIGQUERY_CONNECTOR_JAR=gs://spark-lib/bigquery/spark-bigquery-latest.jar
+gsutil cp $URL_GCS_BIGQUERY_CONNECTOR_JAR $JAR_PATH/
 
 # git clone on Master
 # USER=root
